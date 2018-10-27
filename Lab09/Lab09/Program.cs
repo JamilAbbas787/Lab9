c709426b822439d74251b4db806dd2e55c80d04c
Jamil Abbas <jamilabbas787@gmail.com>
2018-10-27 19:38:20 -0400

    7:45

--- Lab09/Lab09/Program.cs
+++ Lab09/Lab09/Program.cs
@@ -7,19 +7,64 @@ using System.Threading.Tasks;
 namespace Lab09
 {
     class Program
+
     {
+        
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about? \n Enter a number 1-20): ");
-            string student = Console.ReadLine();
-            int.TryParse(student, out int studentNumber);
+            Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about? \n (Enter a number 1-20): ");
 
-            //var student = new
-            //CollectioOfList.PickAStudent(1);
-            Console.WriteLine(CollectioOfList.PickAStudent(studentNumber));
+            uint studentOne = SelectStudent();
+            while (studentOne == 0)
+            {
+                Console.WriteLine("That student does not exist. Please try again. (enter a number 1-20):  ");
+                studentOne = SelectStudent();
+            }
+
+            Console.WriteLine($"Student {studentOne} is {CollectionOfList.PickAStudent(studentOne)}. What would you like to know about {CollectionOfList.PickAStudent(studentOne)}? \n (enter \"hometown\" or \"favorite food\")  ");
+
+
+
+
+
+
 
             Console.ReadKey();
+        }
+
+        public static uint SelectStudent()
+        {
+            bool studentVerification = false;
+            uint studentNumber;
+            string student;
+
+            while (!studentVerification)
+            {
+                
+                student = Console.ReadLine();
+
+                try
+                {
+                    studentVerification = uint.TryParse(student,out studentNumber);
+                }
+                catch (FormatException)
+                {
+                    
+                    Console.WriteLine("That student does not exist. Please try again. (enter a number 1-20):  ");
+                    throw;
+                }
+                if (studentNumber > 20 || studentNumber < 1)
+                {
+                    Console.WriteLine("That student does not exist. Please try again. (enter a number 1-20):  ");
+                    continue;
+                }
+
+                return studentNumber;                                    
+            }
+
+            return 0;
+
         }
     }
 }
--- Lab09/Lab09/students.cs
+++ Lab09/Lab09/students.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lab09
 {
-    class CollectioOfList
+    class CollectionOfList
     {
-        public static string PickAStudent(int numberOnArray)
+        public static string PickAStudent(uint numberOnArray)
         {
             
             var studentNames = new List<KeyValuePair<int, string>>
@@ -34,49 +34,82 @@ namespace Lab09
                 new KeyValuePair<int, string>(19, "Jonathan Ericsson"),
                 new KeyValuePair<int, string>(20, "Frans Nielsen"),
                 };
-
+          
+            try
+            {
+                studentNames.Count();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($" {ex} That student does not exist. Please try again. (enter a number 1 - 20): ");
+            }
 
             var selectionOfStudent = (from val in studentNames where val.Key == numberOnArray select val.Value).FirstOrDefault();
-
             return selectionOfStudent;
-
            } 
-        
-        public static List<string> Hometown(int numberOnArray)
-        {
-            var studentHomeTowns = new List<string>
-            {
-                "Detroit",
-                "Hartford",
-                "Stockholm",
-                "Detroit",
-                "Stockholm",
-                "Toronto",
-                "Toronto",
-                "Baden bin Wein",
-                "Stockholm",
-                "Moscow",
-                "Toronto",
-                "Detroit",
-                "Toronto",
-                "Toronto",
-                "Detroit",
-                "Vancouver",
-                "Winnipeg",
-                "Stockholm",
-                "Stockholm",
-                "Herning",
-                "Toronto",
+       
 
+        public static string Hometown(uint numberOnArray)
+        {
+            var studentHomeTowns = new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(1, "Detroit"),
+                new KeyValuePair<int, string>(2, "Hartford"),
+                new KeyValuePair<int, string>(3, "Stockholm"),
+                new KeyValuePair<int, string>(4, "Detroit"),
+                new KeyValuePair<int, string>(5, "Stockholm"),
+                new KeyValuePair<int, string>(6, "Toronto"),
+                new KeyValuePair<int, string>(7, "Toronto"),
+                new KeyValuePair<int, string>(8, "Baden bin Wein"),
+                new KeyValuePair<int, string>(9, "Stockholm"),
+                new KeyValuePair<int, string>(10,"Moscow"),
+                new KeyValuePair<int, string>(11,"Toronto"),
+                new KeyValuePair<int, string>(12, "Detroit"),
+                new KeyValuePair<int, string>(13, "Toronto"),
+                new KeyValuePair<int, string>(14, "Toronto"),
+                new KeyValuePair<int, string>(15, "Detroit"),
+                new KeyValuePair<int, string>(16, "Vancouver"),
+                new KeyValuePair<int, string>(17, "Winnipeg"),
+                new KeyValuePair<int, string>(18, "Stockholm"),
+                new KeyValuePair<int, string>(19, "Stockholm"),
+                new KeyValuePair<int, string>(20, "Herning")
             };
 
-            return studentHomeTowns;
+            var studentHometown = (from val in studentHomeTowns where val.Key == numberOnArray select val.Value).FirstOrDefault();
+            return studentHometown;
 
         }
 
-        
+        public static string SelectFood(uint numberOnArray)
+        {
+            var food = new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(1, "Cheese Cake"),
+                new KeyValuePair<int, string>(2, "Pasta"),
+                new KeyValuePair<int, string>(3, "Hamburger"),
+                new KeyValuePair<int, string>(4, "Hot Dog"),
+                new KeyValuePair<int, string>(5, "Chinese"),
+                new KeyValuePair<int, string>(6, "Pizza"),
+                new KeyValuePair<int, string>(7, "Fajita"),
+                new KeyValuePair<int, string>(8, "Taco"),
+                new KeyValuePair<int, string>(9, "BBQ"),
+                new KeyValuePair<int, string>(10, "Lobster"),
+                new KeyValuePair<int, string>(11, "Cheese Cake"),
+                new KeyValuePair<int, string>(12, "Pasta"),
+                new KeyValuePair<int, string>(13, "Hamburger"),
+                new KeyValuePair<int, string>(14, "Hot Dog"),
+                new KeyValuePair<int, string>(15, "Chinese"),
+                new KeyValuePair<int, string>(16, "Pizza"),
+                new KeyValuePair<int, string>(17, "Fajita"),
+                new KeyValuePair<int, string>(18, "Taco"),
+                new KeyValuePair<int, string>(19, "BBQ"),
+                new KeyValuePair<int, string>(20, "Lobster")
 
+            };
 
+            var selectedFood = (from val in food where val.Key == numberOnArray select val.Value).FirstOrDefault();
+            return selectedFood;
+        }
 
     }
 }
